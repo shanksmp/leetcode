9f3cfa89e302cc242eaeea9e502ff95c6b13ccf9
Shashank M P <96693770+shanksmp@users.noreply.github.com>
2026-06-16 11:21:09 -0500

    Time: 7 ms (33%), Space: 44.3 MB (74.37%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,6 +1,9 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         int [] alphabet = new int[26];
+        if(s.length() != t.length()){
+            return false;
+        }
         for(int i = 0 ; i<s.length(); i++){
             int value = s.charAt(i) - 'a' ;
             alphabet[value]++;
